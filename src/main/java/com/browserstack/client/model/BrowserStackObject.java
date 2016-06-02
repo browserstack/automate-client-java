393ef10637f24a7458879d10a0ab09fc348727c9
Shirish Kamath <shirishk.87@gmail.com>
2016-06-02 12:40:01 +0530

    Exclude BrowserStackClient from serialization

--- src/main/java/com/browserstack/client/model/BrowserStackObject.java
+++ src/main/java/com/browserstack/client/model/BrowserStackObject.java
@@ -1,6 +1,7 @@
 package com.browserstack.client.model;
 
 import com.browserstack.client.BrowserStackClient;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
 
@@ -8,11 +9,13 @@ import java.io.Serializable;
 public abstract class BrowserStackObject implements Serializable {
     private transient BrowserStackClient client;
 
+    @JsonIgnore
     public BrowserStackClient getClient() {
         return client;
     }
 
     @SuppressWarnings("unchecked")
+    @JsonIgnore
     public <T> T setClient(BrowserStackClient client) {
         this.client = client;
         return (T) this;
