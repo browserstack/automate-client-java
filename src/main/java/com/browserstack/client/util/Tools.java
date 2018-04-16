23d17b560a2d5b370c333e0867cf969908af6a22
Hitesh Raghuvanshi <hitesh@browserstack.com>
2018-04-16 16:11:50 +0530

    adding helper and utility classes

---
+++ src/main/java/com/browserstack/client/util/Tools.java
@@ -0,0 +1,9 @@
+package com.browserstack.client.util;
+
+public class Tools {
+
+  // Checks if a string is null or empty
+  public static boolean isStringEmpty(String str) {
+    return (str == null || str.isEmpty());
+  }
+}
