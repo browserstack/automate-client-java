46d196ad90f68107807f8aa5efbc82b7f3c45b5b
Rohan Chougule <rohan183chougule@gmail.com>
2020-09-15 17:24:00 +0530

    chore: removed unused imports

--- src/main/java/com/browserstack/client/util/Tools.java
+++ src/main/java/com/browserstack/client/util/Tools.java
@@ -1,14 +1,10 @@
 package com.browserstack.client.util;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 public class Tools {
 
-  // Checks if a string is null or empty
-  public static boolean isStringEmpty(String str) {
-    return (str == null || str.isEmpty());
-  }
+    // Checks if a string is null or empty
+    public static boolean isStringEmpty(String str) {
+        return (str == null || str.isEmpty());
+    }
 
 }
